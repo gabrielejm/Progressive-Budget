c83d0aeb9cb11940967340fb682a44766c0adb6d
Justin Gabriele <gabrielejustin@yahoo.com>
2021-01-22 13:24:00 -0500

    console errors gone, changed to service-worker in serverworker reg

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,13 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
-  "/index.html",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
-  "/styles.css",
-  "/index.js",
-  "/models/transaction.js",
-  "manifest.webmanifest",
+  "/public/index.html",
+  "/public/index.js",
+  "/public/manifest.webmanifest",
+  "/public/styles.css",
+  "/public/icons/icon-192x192.png",
+  "/public/icons/icon-512x512.png",
+  // "/models/transaction.js",
 ];
 
 // install
