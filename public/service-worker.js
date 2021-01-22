63d7c30d31fd4238c71d346d2d6d0a82d5ca2275
Justin Gabriele <gabrielejustin@yahoo.com>
2021-01-22 11:40:55 -0500

    added files to cache to serviceworker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,15 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
-const FILES_TO_CACHE = ["/"];
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/styles.css",
+  "/index.js",
+  "/routes/api.js",
+  "/models/transaction.js",
+];
 
 // install
 self.addEventListener("install", function (evt) {
