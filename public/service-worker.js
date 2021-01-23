81f9050059482a9f29d94dd82e8f55ac28c418d6
Justin Gabriele <gabrielejustin@yahoo.com>
2021-01-23 09:42:59 -0500

    removed index.html from service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "/",
-  "index.html",
+  // "index.html",
   "index.js",
   "manifest.webmanifest",
   "styles.css",
